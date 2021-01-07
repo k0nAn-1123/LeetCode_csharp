6e866b5af93b1de362aae388e355a59650d1acff
zhaoxiang <zhaoxiang@henhaoji.com>
2021-01-07 20:14:21 +0800

    2020-01-07 提交treenode类

---
+++ Models/TreeNode.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.Models
+{
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+        public TreeNode(int x) { val = x; }
+    }
+}
